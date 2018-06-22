6d6e536a3a35a08a3ffbf8355da2617cb821d7f5
John Ingram <j123j7@gmail.com>
2018-06-22 02:55:31 +0200

    Levels 1, 2 complete
    Creaded Music handler that plays random songs from a list of music, and stays loaded between scenes
    Added some textures and stuff also

---
+++ Assets/scripts/Music_Randomizer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music_Randomizer : MonoBehaviour {
+    public AudioSource audioSource;
+    public AudioClip[] clips;
+    public float timeBetweenClips;
+
+    private int playedClip;
+    private int play;
+
+    private void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        Play();
+    }
+    void Update () {
+
+        if (!audioSource.isPlaying)
+        {
+
+            
+            if (playedClip != play)
+            {
+                StartCoroutine(Pause());
+            }
+        }
+	}
+    IEnumerator Pause()
+    {
+        yield return new WaitForSeconds(timeBetweenClips);
+        Play();
+    }
+    private void Play()
+    {
+        play = Random.Range(0, clips.Length);
+        audioSource.clip = clips[play];
+        playedClip = play;
+        audioSource.Play();
+    }
+}

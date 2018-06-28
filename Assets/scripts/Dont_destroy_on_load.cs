1a90f025d0d92c3280bffca02215a63898deeb19
John Ingram <j123j7@gmail.com>
2018-06-29 01:13:28 +0200

    Changed the music to the stuff i payed for
    also removed music_randomizer, cause this loops

---
+++ Assets/scripts/Dont_destroy_on_load.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dont_destroy_on_load : MonoBehaviour {
+
+	
+	void Start () {
+        DontDestroyOnLoad(this.gameObject);
+    }
+	
+}
